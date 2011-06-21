388d291126e50d1eb4d995629fc196a8709bf924
tv <tv+mway@also>
2011-06-21 13:16:27 +0200

    Generate a file in index.html

--- modules/index.html/index.js
+++ modules/index.html/index.js
@@ -3,12 +3,13 @@
 
 jsdom = require('jsdom');
 
-exports.deps = [ 'config', 'cache.manifest', 'scripts', 'styles' ];
+exports.deps = [ 'config', 'files', 'cache.manifest', 'scripts', 'styles' ];
 
 exports.duty = function (callback) {
   var config = this.config;
   var scripts = this.scripts;
   var styles = this.styles;
+  var files = this.files;
   var jquery_uri = '../../frameworks/The-M-Project/modules/jquery/jquery-1.6.1.min.js';
 
   //
@@ -47,6 +48,12 @@ exports.duty = function (callback) {
       };
     });
 
-    callback('<!DOCTYPE html>' + window.document.innerHTML);
+    files['index.html'] = {
+      requestPath: 'index.html',
+      type: 'text/html',
+      content: '<!DOCTYPE html>' + window.document.innerHTML
+    };
+
+    callback();
   });
 };
