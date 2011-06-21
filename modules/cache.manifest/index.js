b35d46f7b965b9065182bebd3a27b5394b29b645
tv <tv+mway@also>
2011-06-21 10:44:48 +0200

    Add cache.manifest module

---
+++ modules/cache.manifest/index.js
@@ -0,0 +1,53 @@
+
+// module: cache.manifest
+
+exports.deps = [ 'config', 'files' ];
+
+exports.duty = function (callback) {
+  var config = this.config;
+  var files = this.files;
+
+  var content = [];
+
+  function log () {
+    //console.log.apply(this, arguments);
+    content.push(Array.prototype.slice.call(arguments).join(' '));
+  };
+
+  log('CACHE MANIFEST');
+  log('# Application: ' + config.name
+      + ', Version: ' + config.version
+      + ', Timestamp: ' + config.buildVersion);
+
+  // Explicitly cached entries
+  log('');
+  log('CACHE:');
+  // TODO read excludedFromCaching...
+  Object.keys(files).forEach(function (filename) {
+    var file = files[filename];
+    if (file.href) {
+      log(file.href);
+    };
+  });
+
+  // Resources that require the device/user to be online.
+  log('');
+  log('NETWORK:');
+  // TODO proxies -> no, we got *
+  // TODO excludedFromCaching -> no, we got *
+
+  // enable wildcard. 
+  log('*');
+
+  log('');
+  log('FALLBACK:');
+  // TODO this.manifest.fallback = config.cacheFallbacks;
+
+  files['cache.manifest'] = {
+    content: content.join('\r\n'),
+    type: 'text/cache-manifest',
+    href: 'cache.manifest'
+  };
+
+  callback();
+};
