a5b680910218c6a8ca019d017fe5ffdc1a51156c
tv <tv+mway@also>
2011-06-22 10:48:13 +0200

    Push script when type is already correct

--- modules/scripts/index.js
+++ modules/scripts/index.js
@@ -9,18 +9,22 @@ exports.duty = function (callback) {
   var scripts = this.config.scripts;
 
   Object.keys(files).forEach(function (filename) {
-    var match = /\.([^.]+)$/.exec(filename);
-    if (match) {
-      var ext = match[1];
-      // TODO put file into some cache
-      // TODO put this into a Content-Type-detection module
-      switch (ext) {
-        case 'js':
-          var file = files[filename];
-          file.type = 'application/javascript';
-          file.requestPath = require('path').basename(filename);
-          scripts.push(file);
-          break;
+    var file = files[filename];
+    if (file.type === 'application/javascript') {
+      scripts.push(file);
+    } else {
+      var match = /\.([^.]+)$/.exec(filename);
+      if (match) {
+        var ext = match[1];
+        // TODO put file into some cache
+        // TODO put this into a Content-Type-detection module
+        switch (ext) {
+          case 'js':
+            file.type = 'application/javascript';
+            file.requestPath = require('path').basename(filename);
+            scripts.push(file);
+            break;
+        };
       };
     };
   });
