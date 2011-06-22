306dc93a16c1f5f21a51d7be80010ff2afba49a9
tv <tv+mway@also>
2011-06-22 10:43:20 +0200

    Add 'virtual' files {{{app:{prologue,epilogue}}}}

--- modules/app/index.js
+++ modules/app/index.js
@@ -10,10 +10,57 @@ exports.duty = function (callback) {
   var files = this.config.files;
 
   var context = {
+    self: this.self,
     config: {
       files: {}
     }
   };
+
+  var prologue = {
+    filename: '{{{app:prologue}}}',
+    framework: 'app/prologue', //this.self.name, // self.name = 'app' vs. config.name = ...
+    is_root_file: true,
+    requestPath: null,
+    type: 'application/javascript',
+    analysis: {
+      definitions: [
+        this.config.name,
+        'M.Application.name'
+      ],
+      references: [
+        'M.Application',
+        'M'
+      ]
+    },
+    content: [
+      'var ' + this.config.name + '=' + this.config.name + '||{}',
+      'M.Application.name=' + JSON.stringify(this.config.name)
+    ].join(';')
+  };
+
+  var epilogue = {
+    filename: '{{{app:epilogue}}}',
+    framework: 'app/epilogue',
+    is_root_file: true,
+    requestPath: null,
+    type: 'application/javascript',
+    analysis: {
+      definitions: [],
+      references: [
+        this.config.name,
+        this.config.name + '.app',
+        this.config.name + '.app.main'
+      ]
+    },
+    content: [
+      this.config.name + '.app.main()'
+    ].join(';')
+  };
+
+  var app_name = this.config.name;
+
+  files[prologue.filename] = prologue;
+  files[epilogue.filename] = epilogue;
 
   // "mark root files"
   require('../../lib/espresso_utils').collectFiles.call(context,
