a60adf269d2613cdb39144cdee33463a5bc4bd79
tv <tv+mway@also>
2011-06-24 14:22:22 +0200

    Do not mangle context in build

--- modules/build/index.js
+++ modules/build/index.js
@@ -16,30 +16,5 @@ exports.deps = [
 ];
 
 exports.duty = function (callback) {
-
-  console.log();
-
-  (function walk (that) {
-    if (that instanceof Object) {
-      Object.keys(that).forEach(function (x) {
-        if (that[x] instanceof Object) {
-          if (that[x] instanceof Buffer || that[x] instanceof Job) {
-            that[x] = '[...]';
-          } else {
-            [ 'tree', 'content' ].forEach(function (y) {
-              if (y in that[x]) {
-                that[x][y] = '[...]';
-              };
-            });
-            walk(that[x]);
-          };
-        };
-      });
-    };
-  })(this);
-
-  console.log('== summary ==');
-
-  console.log(require('util').inspect(this, false, 10));
   callback();
 };
