d7faad5f4d47bcdad3b814a6863621eb51d6febc
tv <tv+mway@also>
2011-06-24 16:05:26 +0200

    Trigger rebuild on every index.html request

--- modules/server/index.js
+++ modules/server/index.js
@@ -1,31 +1,23 @@
 
 // module: server
 
-exports.deps = [ 'config', 'build', 'files', 'config', 'proxies' ];
+exports.deps = [ 'defaults', 'config', 'proxies' ];
 
 var http = require('http'),
     parse = require('url').parse,
     format = require('url').format,
     join = require('path').join;
 
+var schedule = require('slow-job-scheduler').schedule;
+
 exports.duty = function (callback) {
+  var defaults = this.defaults;
   var config = this.config;
   var proxies = this.proxies;
   var prefix = '/' + config.name;
 
-  // create file table
-  var files = this.files;
   var file_table = {};
-  Object.keys(this.files).forEach(function (filename) {
-    var file = files[filename];
-    if (typeof file.requestPath === 'string') {
-      var requestPath = join(prefix, file.requestPath);
-      file_table[requestPath] = file;
-    };
-  });
 
-  // TODO trigger rebuild
-  
   http.createServer(function (req, res) {
     var url = parse(req.url, true);
 
@@ -36,24 +28,35 @@ exports.duty = function (callback) {
         url.pathname += 'index.html';
         req.url = format(url);
     };
-  
-    // dispatch
-    if (url.pathname in file_table) {
-      var file = file_table[url.pathname];
-  
-      var content = file.content;
-      if (!(content instanceof Buffer)) {
-        content = new Buffer(file.content);
-      };
-      content.type = file.type;
-  
-      console.log('serve file:', url.pathname, content.type, content.length);
-      res.writeHead(200, 'OK', {
-        'Content-Length': content.length,
-        'Content-Type': content.type
+
+    console.log('[35;1mrequest:', url.pathname, '[m');
+
+    if (url.pathname === prefix + '/index.html') {
+      // build or rebuild file_table
+
+      // TODO honor -m parameter
+      defaults.offlineManifest = false;
+
+      schedule([ 'build', 'files' ]).run(function (callback) {
+
+        // create file table
+        file_table = {};
+        var files = this.files;
+        Object.keys(this.files).forEach(function (filename) {
+          var file = files[filename];
+          if (typeof file.requestPath === 'string') {
+            var requestPath = join(prefix, file.requestPath);
+            file_table[requestPath] = file;
+          };
+        });
+
+        dispatch(req, res);
       });
-      res.end(content);
-    } else if (!proxies.handle(req, res)) {
+    } else if (proxies.handle(req, res)) {
+      /* nothing to do */
+    } else if (dispatch(req, res)) {
+      /* nothing to do */
+    } else {
       var content = 'Not found: ' + url.pathname;
       console.log(content);
       res.writeHead(404, 'Not Found', {
@@ -66,4 +69,25 @@ exports.duty = function (callback) {
     console.log('Server running at',
       'http://' + config.hostname + ':' + config.port + prefix);
   });
+
+  function dispatch (req, res) {
+    var url = parse(req.url, true);
+    if (url.pathname in file_table) {
+      var file = file_table[url.pathname];
+
+      var content = file.content;
+      if (!(content instanceof Buffer)) {
+        content = new Buffer(file.content);
+      };
+      content.type = file.type;
+
+      console.log('serve file:', url.pathname, content.type, content.length);
+      res.writeHead(200, 'OK', {
+        'Content-Length': content.length,
+        'Content-Type': content.type
+      });
+      res.end(content);
+      return true;
+    };
+  };
 };
