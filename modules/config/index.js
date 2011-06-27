4246b660b7b21515ee98f114608b2f6b030e3df0
tv <tv+mway@also>
2011-06-27 17:38:15 +0200

    Use defaults as config.prototype

--- modules/config/index.js
+++ modules/config/index.js
@@ -5,12 +5,19 @@ exports.deps = [ 'defaults', 'config.json', 'command-line' ];
 
 exports.duty = function (callback) {
   var that = this;
-  var config = {};
-  exports.deps.forEach(function (name) {
+
+  var tail = exports.deps.slice();
+  var head = tail.shift();
+
+  // Use defaults as a base and copy properties from config.json, command-line.
+  // We do this, so other modules can overwrite defaults.
+  var config = Object.create(that[head]);
+  tail.forEach(function (name) {
     for (key in that[name]) {
       config[key] = that[name][key];
     };
   });
-  console.log('config:', JSON.stringify(config, null, 2));
+
+  //console.log('config:', JSON.stringify(config, null, 2));
   return callback(config);
 };
